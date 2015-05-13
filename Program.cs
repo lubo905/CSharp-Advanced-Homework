6f6b7ef4750e8162f7e8c2bad766551dc37a1416
lubo905 <lubo__90@mail.bg>
2015-05-13 19:27:01 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+ class Program
+    {
+        static void Main()
+        {
+            string numbers = Console.ReadLine();
+            string[] items = numbers.Split(' ');
+            Array.Sort(items);
+
+            foreach (var value in items)
+            {
+                Console.WriteLine(value);
+               
+            }
+         
+        }
+
+    }
